7e8e7107c58f32145924648a2243841c54dadc50
Fernando Vega <pytness@gmail.es>
2024-06-27 16:07:33 +0200

    Add partial implementation for multi button press

--- main.cpp
+++ main.cpp
@@ -234,10 +234,14 @@ void PCF8574::selectN(const uint8_t pin) {
 	write8(n);
 };
 
+
 #define ERROR_LED 11
 #define TEST_INPUT 12
 
-const byte ADDRESS_COUNT_LIMIT = 4;
+#define BUTTON_BUFFER_LENGTH 8 
+#define NO_BUTTON -1
+
+byte ADDRESS_COUNT_LIMIT = 4;
 
 const byte ROW_ADDRESS_START = 0x20;
 const byte COLUMN_ADDRESS_START = 0x24;
@@ -245,28 +249,39 @@ const byte COLUMN_ADDRESS_START = 0x24;
 int ROW_ADDRESS_COUNT = 0;
 int COLUMN_ADDRESS_COUNT = 0;
 
+int BUTTON_BUFFER[BUTTON_BUFFER_LENGTH] = {}; 
+
 Adafruit_LiquidCrystal lcd(0);
 
-void discover_row_col_devices() {
-	for (int i = ROW_ADDRESS_START; i <= (ROW_ADDRESS_START + ADDRESS_COUNT_LIMIT); i++) {
+void reset_button_buffer() {
+	for (int i = 0; i < BUTTON_BUFFER_LENGTH; i++) {
+		BUTTON_BUFFER[i] = NO_BUTTON;
+	}
+}
+
+int discover_line(int from, int to) {
+	int count = 0;
+
+	for (int i = ROW_ADDRESS_START; i <= ROW_ADDRESS_END; i++) {
 		Wire.beginTransmission(i);
 		if (Wire.endTransmission() == 0) {
-			ROW_ADDRESS_COUNT++;
+			count++;
 			delay(1);
 		} else {
 			break;
 		}
 	}
 
-	for (int i = COLUMN_ADDRESS_START; i <= (COLUMN_ADDRESS_START + ADDRESS_COUNT_LIMIT); i++) {
-		Wire.beginTransmission(i);
-		if (Wire.endTransmission() == 0) {
-			COLUMN_ADDRESS_COUNT++;
-			delay(1);
-		} else {
-			break;
-		}
-	}
+	return count;
+}
+
+void discover_row_col_devices() {
+
+	const int ROW_ADDRESS_END = ROW_ADDRESS_START + ADDRESS_COUNT_LIMIT;
+	const int COLUMN_ADDRESS_END = COLUMN_ADDRESS_START + ADDRESS_COUNT_LIMIT;
+
+	ROW_ADDRESS_COUNT = discover_line(ROW_ADDRESS_START, ROW_ADDRESS_END); 
+	COLUMN_ADDRESS_COUNT = discover_line(COLUMN_ADDRESS_START, COLUMN_ADDRESS_END); 
 
 	Serial.println(ROW_ADDRESS_COUNT);
 	Serial.println(COLUMN_ADDRESS_COUNT);
@@ -285,20 +300,21 @@ int read_buttons_at_address(int row_address, int column_address) {
 	const int row_chunk_index = row_address - ROW_ADDRESS_START;
 	const int column_chunk_index = column_address - COLUMN_ADDRESS_START;
 
-
 	ROWS_PCF.write8(0);
 
 	const int cols_status = COLS_PCF.read8();
 
 	// Reset to HIGH
-	ROWS_PCF.write8(255);
+	ROWS_PCF.write8(0xff);
 
-	if (cols_status == 255)	{
+	// No button pressed detected anywhere
+	if (cols_status == 0xff) {
 		return result;
 	}
 
-	for (byte column = 0; column <= 7; column++) {
+	// TODO: shift cols_status and & with rows_status to extract
 
+	for (byte column = 0; column <= 7; column++) {
 		// All bits are inverted, so looking for a LOW bit
 		const bool is_activated = get_bit(cols_status, column) == false;
 
@@ -333,20 +349,33 @@ int read_buttons_at_address(int row_address, int column_address) {
 	return result;
 }
 
-
 int read_buttons() {
-	for(int row_address = ROW_ADDRESS_START; row_address < (ROW_ADDRESS_START + ROW_ADDRESS_COUNT); row_address++) {
-		for(int column_address = COLUMN_ADDRESS_START; column_address < (COLUMN_ADDRESS_START + COLUMN_ADDRESS_COUNT); column_address++) {
+
+	reset_button_buffer();
+
+	const int ROW_ADDRESS_END = ROW_ADDRESS_START + ROW_ADDRESS_COUNT;
+	const int COLUMN_ADDRESS_END = COLUMN_ADDRESS_START + COLUMN_ADDRESS_COUNT;
+
+	int count = 0; 
+
+
+	for (int row_address = ROW_ADDRESS_START; row_address < ROW_ADDRESS_END; row_address++) {
+		for (int column_address = COLUMN_ADDRESS_START; column_address < COLUMN_ADDRESS_END; column_address++) {
+
+			if (count == BUTTON_BUFFER_LENGTH) {
+				return count;
+			}
 
 			int button = read_buttons_at_address(row_address, column_address);
 
 			if (button != -1) {
-				return button;
+				BUTTON_BUFFER[count] = button;
+				count += 1;
 			}
 		}
 	}
 
-	return -1;
+	return count;
 }
 
 void test_mode() {
@@ -384,6 +413,8 @@ void write_to_alphanum(int value) {
 }
 
 void setup() {
+
+	reset_button_buffer();
 	Serial.begin(115200);
 
 	pinMode(ERROR_LED, OUTPUT);
@@ -418,10 +449,13 @@ void setup() {
 
 int last_button = -1;
 void loop() {
-	int const result = read_buttons();
+	int const button_count = read_buttons();
 
-	if (result != last_button) {
+
+	for (int i = 0; i < button_count; i++) {
+		int button = BUTTON_BUFFER[i];
 		last_button = result;
+
 		write_to_alphanum(result);
 		Serial.print("button # ");
 		Serial.print(result);
